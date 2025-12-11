0f73d0fa6ee0893681cb475934cce7919720b94b
phumsiri-s-omise <phumsiri.s@opn.ooo>
2025-12-11 15:13:03 +0700

    Upgrade omise-php from 2.18.0 to 3.0.0 and update API version to 2019-05-29

--- Plugin/ConfigSectionPaymentPlugin.php
+++ Plugin/ConfigSectionPaymentPlugin.php
@@ -68,8 +68,8 @@ class ConfigSectionPaymentPlugin
         $this->helper = $helper;
         $this->messageManager = $messageManager;
         $this->scopeConfig = $scopeConfig;
-        // using same version as omise-php 2.13(2017-11-02)
-        define('OMISE_API_VERSION', '2017-11-02');
+        // using same version as omise-php 3.0.0 (2019-05-29)
+        define('OMISE_API_VERSION', '2019-05-29');
     }
 
     /**
